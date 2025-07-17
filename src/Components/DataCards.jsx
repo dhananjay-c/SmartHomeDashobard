202f3a12aca5ad43fec5bf74655849cfd7410d96
Dhananjay Chavan <dhananjaychavan0907@gmail.com>
2025-07-17 13:41:28 +0530

    Update DataCards.jsx

--- src/Components/DataCards.jsx
+++ src/Components/DataCards.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-// import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const DataCards = ({ color, icon, title, value }) => {
 
-    // const theme = useSelector((state) => state.theme);
+    const theme = useSelector((state) => state.theme);
 
     return (
         <>
-            <div className='w-42 h-35 px-3 py-7 rounded-4xl text-[rgb(31,31,31)]' style={{ background: color }}>
+            <div className='w-42 h-35 px-3 py-7 rounded-4xl text-[rgb(31,31,31)]' style={{ background: color, boxShadow: `0 0 12px ${theme.shadow}` }}>
                 <div className='flex items-center'>
                     <div>{icon}</div> <h1 className='text-sm font-medium ml-2'>{title}</h1>
                 </div>
